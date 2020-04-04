4670b780bd15142a0fea0a2cfe7c3836efe3a4b5
Armen Grigorian <info@propertystart.com.au>
2020-04-04 11:43:07 +1100

    Version 4

--- data.js
+++ data.js
@@ -24,16 +24,16 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -0.02315232681216628,
-        "pitch": 0.010014311410952104,
+        "yaw": 0,
+        "pitch": 0,
         "fov": 1.3219339076578436
       },
       "linkHotspots": [
         {
-          "yaw": 0.9613509635687087,
-          "pitch": 0.2381150073644882,
-          "rotation": 13.351768777756625,
-          "target": "2-master"
+          "yaw": 0.9717944366311038,
+          "pitch": 0.2435088252689237,
+          "rotation": 0.7853981633974483,
+          "target": "3-master"
         }
       ],
       "infoHotspots": []
@@ -62,22 +62,46 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -2.9107734350749226,
-        "pitch": 0.03915939917508027,
+        "yaw": -1.9727875054947859,
+        "pitch": 0.1556538457533172,
         "fov": 1.3219339076578436
       },
-      "linkHotspots": [
-        {
-          "yaw": 2.0831542454954937,
-          "pitch": 0.23506277961190492,
-          "rotation": 0.7853981633974483,
-          "target": "2-master"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "2-master",
+      "id": "2-back-yard",
+      "name": "Back Yard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-master",
       "name": "Master",
       "levels": [
         {
@@ -100,15 +124,22 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -0.2991993003418756,
+        "pitch": 0.0575139935166753,
+        "fov": 1.3219339076578436
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.3550304601850893,
+          "pitch": 0.25816834709796055,
+          "rotation": 2.356194490192345,
+          "target": "1-kitchen"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "3-bedroom-2",
+      "id": "4-bedroom-2",
       "name": "Bedroom 2",
       "levels": [
         {
@@ -131,15 +162,15 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": -0.25783523759083593,
-        "pitch": 0.12706136233229692,
-        "fov": 1.3219339076578436
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     },
     {
-      "id": "4-bedroom-3",
+      "id": "5-bedroom-3",
       "name": "Bedroom 3",
       "levels": [
         {
@@ -162,53 +193,15 @@ var APP_DATA = {
       ],
       "faceSize": 1344,
       "initialViewParameters": {
-        "yaw": 1.9066225242290802,
-        "pitch": 0.019621917860675353,
+        "yaw": -1.3365714572083363,
+        "pitch": 0.1912945670198738,
         "fov": 1.3219339076578436
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.6167162917046802,
-          "pitch": 0.19861889753843087,
-          "rotation": 0,
-          "target": "0-entry"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-backyard",
-      "name": "Backyard",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "32 Brushbox Ct Clayton",
+  "name": "32 Brushbox Court, Clayton, Vic 3168",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
